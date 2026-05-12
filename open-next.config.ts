247197b632169138e040772382f6dfc6a0d5d1c3
thanhxnam2005 <thanhxnam2005@gmail.com>
2026-05-13 01:51:27 +0700

    fix: rename proxy.ts to middleware.ts for OpenNext compat (proxy.ts not supported yet)

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,12 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-});
+export default defineCloudflareConfig({});
